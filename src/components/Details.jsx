e14f52ac99aa099721bd5c87380ec3b45402c2fd
amdbattung <anthony@amdbattung.com>
2026-01-16 16:01:17 +0800

    Make details scrollable

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -27,11 +27,11 @@ function Details() {
     };
 
     return (
-        <section className="m-5 p-5 flex items-center justify-center overflow-hidden animate-appear delay-100 fill-mode-forwards opacity-0">
+        <section className="m-5 p-5 flex items-center justify-center animate-appear delay-100 fill-mode-forwards opacity-0">
             <button className="disabled:opacity-0" onClick={prevItem} disabled={ currentIndex < items.length - 1 }>
                 <LeftArrow className="fill-white w-9 h-min" />
             </button>
-            <div className="w-full flex flex-col items-center p-7 gap-8">
+            <div className="w-full flex flex-col max-h-[85vh] overflow-y-auto items-center p-7 gap-8">
                 {items[currentIndex]}
             </div>
             <button className="disabled:opacity-0" onClick={nextItem} disabled={ currentIndex >= 1 }>
